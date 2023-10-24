a1b07a417f8242574d7531e60c3c479778addb4c
fatmahattab <114833756+fatmahattab@users.noreply.github.com>
2023-10-24 18:38:16 +0000

    Add files via upload

---
+++ users-microservice/src/main/java/com/fatma/users/service/UserService.java
@@ -0,0 +1,26 @@
+package com.fatma.users.service;
+
+import com.fatma.users.entities.Role;
+import com.fatma.users.entities.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    User findUserByUsername (String username);
+    Role addRole(Role role);
+    User addRoleToUser(long id, Role r);
+
+    List<User> findAllUsers();
+
+    User findUserById(Long id);
+
+    List<Role> findAllRoles();
+
+    Role findRoleById(Long id);
+
+    void deleteUser(long id);
+
+    User removeRoleFromUser(long id, Role r);
+}
+
